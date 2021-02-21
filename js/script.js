d4408c4ebfb680b062ac492499c2048eeb490050
Giovanni <giovannipor23@gmail.com>
2021-02-22 00:12:51 +0100

    Bonus

--- js/script.js
+++ js/script.js
@@ -5,31 +5,45 @@ const sconto40 = 0.6;
 
 var km = prompt('Quanti km devi percorrere?');
 console.log(km);
-document.getElementById('km').innerHTML = km;
 
-var eta = prompt('Quanti anni hai?');
-console.log(eta);
-document.getElementById('eta').innerHTML = eta;
-
-
-if (eta < 18) {
-    var costo = km * euroPerKm * sconto20;
-    document.getElementById('sconto').innerHTML = '20%';
-
-}
-
-else if (eta > 64) {
-    var costo = km * euroPerKm * sconto40;
-    document.getElementById('sconto').innerHTML = '40%';
-
+if (isNaN(km)) {
+    alert('Inserisci un valore numerico');
+    document.getElementById('km').innerHTML = 'inserimento non valido';
 }
 
 else {
-    var costo = km * euroPerKm;
-    document.getElementById('sconto').innerHTML = 'Nessuno sconto applicabile';
+    document.getElementById('km').innerHTML = km;
+    var eta = prompt('Quanti anni hai?');
+    console.log(eta);
 
+    if (isNaN(eta)) {
+        alert('Inserisci un valore numerico');
+        document.getElementById('eta').innerHTML = 'inserimento non valido';
+    }
+
+    else {
+        document.getElementById('eta').innerHTML = eta;
+
+        if (eta < 18) {
+            var costo = km * euroPerKm * sconto20;
+            document.getElementById('sconto').innerHTML = '20%';
+        
+        }
+        
+        else if (eta > 64) {
+            var costo = km * euroPerKm * sconto40;
+            document.getElementById('sconto').innerHTML = '40%';
+        
+        }
+        
+        else {
+            var costo = km * euroPerKm;
+            document.getElementById('sconto').innerHTML = 'Nessuno sconto applicabile';
+        
+        }
+
+        console.log(costo);
+        document.getElementById('costo').innerHTML = costo.toFixed(2)+' '+valuta;
+
+    }
 }
-
-console.log(costo);
-document.getElementById('costo').innerHTML = costo+' '+valuta;
-
